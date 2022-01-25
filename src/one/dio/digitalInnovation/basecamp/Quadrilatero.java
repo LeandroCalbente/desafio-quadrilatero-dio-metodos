10ec341423371d51f253531743d934f0ed58c47a
LeandroCalbente <leandro_calbente@hotmail.com>
2022-01-25 20:28:21 -0300

    first commit

--- src/one/dio/digitalInnovation/basecamp/Quadrilatero.java
+++ src/one/dio/digitalInnovation/basecamp/Quadrilatero.java
@@ -11,7 +11,7 @@ public class Quadrilatero {
 	
 	public static void area(double lado1, double lado2) {
 		
-		System.out.println("Área do retâgulo: " + lado1 * lado2);
+		System.out.println("Área do retângulo: " + lado1 * lado2);
 		
 		
 	}
