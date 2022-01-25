bc80d9be74ea028e0369210f679fc55c8c61f823
LeandroCalbente <leandro_calbente@hotmail.com>
2022-01-25 15:22:42 -0300

    first commit

---
+++ src/one/dio/digitalInnovation/basecamp/Main.java
@@ -0,0 +1,17 @@
+package one.dio.digitalInnovation.basecamp;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		//Quadrilátero
+		
+		System.out.println("Exercício quadrilátero");
+		
+		Quadrilatero.area(3);
+		Quadrilatero.area(5, 5);
+		Quadrilatero.area(7, 8, 9);
+		
+	}
+
+}
